f984b6673b4787db3df69d3922931a3f9d7aca54
lgants <lgants@gmail.com>
2016-10-22 23:15:06 -0400

    lab complete

--- lib/queue.rb
+++ lib/queue.rb
@@ -2,4 +2,22 @@ require_relative 'stack'
 
 class Queue
 
+  def initialize
+    @in_stack = Stack.new
+    @out_stack = Stack.new
+  end
+
+  def enqueue(element)
+    @in_stack.push(element)
+  end
+
+  def dequeue 
+    if @out_stack.size == 0
+      while @in_stack.size > 0
+        @out_stack.push(@in_stack.pop)
+      end
+    end
+    @out_stack.pop
+  end
+
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,20 @@
 class Stack
 
+  def initialize
+    @elements = []
+  end
+
+  def push(element)
+    @elements.push(element)
+    self
+  end
+
+  def pop
+    @elements.pop
+  end
+
+  def size
+    @elements.size
+  end
+
 end
